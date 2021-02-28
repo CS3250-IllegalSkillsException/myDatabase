b7b897026a4b5b3b1527de92ef8308d78ed912f0
Megan Stanford <mestanford88@gmail.com>
2021-02-28 05:36:54 -0700

    Added new search function with compatibility for both db tables
    A lot of these changes seem to just be from formatting. Primary change is the addition of the Search function, which splits into 2 sub-menus: 1 for inventory table and 1 for orders table.

--- Main.java
+++ Main.java
@@ -2,51 +2,51 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		menuLoop();		
+		menuLoop();
 	}
 	static Scanner input = new Scanner(System.in);
 
 	public static void menuLoop() {
 		String error = "error";
 		while(error.contentEquals("error")) {
-		System.out.println("Welcome to the inventory database editor.");
-		System.out.println("Would you like to use a database other than the local database? (Y/N) Enter X to quit.");
-		String csvReply = input.nextLine();
+			System.out.println("Welcome to the inventory database editor.");
+			System.out.println("Would you like to use a database other than the local database? (Y/N) Enter X to quit.");
+			String csvReply = input.nextLine();
 
-		if (csvReply.contentEquals("Y")){
-			System.out.println("Please provide login info");
-			Scanner input = new Scanner(System.in);
-			System.out.println("Username: ");
-			String username = input.nextLine();
-			System.out.println("Password: ");
-			String password = input.nextLine();
-			System.out.println("Database URL: ");
-			String url = input.nextLine();
-			Database db = new Database(username, password, url);
-			optionLoop(db);
-		} else if (csvReply.contentEquals("N")) {
-			System.out.println("Please provide login info");
-			Scanner input = new Scanner(System.in);
-			System.out.println("Username: ");
-			String username = input.nextLine();
-			System.out.println("Password: ");
-			String password = input.nextLine();
-			Database db = new Database(username, password);
-			optionLoop(db);
-		} else if (csvReply.contentEquals("X")) {
-			System.out.println("Exiting program...");
-			break;
-		} else {
-			error = "error";
-			System.out.println("Invalid Response. Please enter Y or N.");
+			if (csvReply.contentEquals("Y")){
+				System.out.println("Please provide login info");
+				Scanner input = new Scanner(System.in);
+				System.out.println("Username: ");
+				String username = input.nextLine();
+				System.out.println("Password: ");
+				String password = input.nextLine();
+				System.out.println("Database URL: ");
+				String url = input.nextLine();
+				Database db = new Database(username, password, url);
+				optionLoop(db);
+			} else if (csvReply.contentEquals("N")) {
+				System.out.println("Please provide login info");
+				Scanner input = new Scanner(System.in);
+				System.out.println("Username: ");
+				String username = input.nextLine();
+				System.out.println("Password: ");
+				String password = input.nextLine();
+				Database db = new Database(username, password);
+				optionLoop(db);
+			} else if (csvReply.contentEquals("X")) {
+				System.out.println("Exiting program...");
+				break;
+			} else {
+				error = "error";
+				System.out.println("Invalid Response. Please enter Y or N.");
+			}
 		}
-	}
 	}
 	public static void optionLoop(Database db) {
 		String notDone = "";
 		while(notDone != "N") {
-		System.out.println("Would you like to edit your CSV file? (Y/N)");
-		String editReply = input.nextLine();
+			System.out.println("Would you like to edit your CSV file? (Y/N)");
+			String editReply = input.nextLine();
 			if (editReply.contentEquals("Y")) {
 				notDone = "";
 				System.out.println("Please enter one of the following options.");
@@ -55,6 +55,7 @@ public class Main {
 				System.out.println("[D] to delete an entry");
 				System.out.println("[M] to modify an entry");
 				System.out.println("[R] to read an entry");
+				System.out.println("[G] to generate report with filters");
 				System.out.println("[S] to simulate customer orders");
 				String editOption = input.nextLine();
 				if(editOption.contentEquals("Q")) {
@@ -85,19 +86,22 @@ public class Main {
 				} else if(editOption.contentEquals("R")) {
 					System.out.println("-----------Read Entry-----------");
 					db.read();
+				} else if(editOption.contentEquals("G")) {
+					System.out.println("-----------Generate Report-----------");
+					db.search();
 				} else if(editOption.contentEquals("S")) {
 					System.out.println("-----------Simulating orders-----------");
 					db.importCustomerData();
 					System.out.println("-----------Complete!-----------");
 				} else {
 					System.out.println("Invalid Response. Please enter a valid option.");
-					}
+				}
 			} else if (editReply.contentEquals("N")) {
 				notDone = "N";
 			} else {
 				System.out.println("Invalid Response. Please enter Y or N.");
-				}
 			}
-			db.closeConnection(); //Cleans up out connection
 		}
+		db.closeConnection(); //Cleans up out connection
 	}
+}
