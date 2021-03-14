964388b89a7bf1efb34e5a2851646ceae998c0c0
Richard <richardoh95@gmail.com>
2021-03-14 08:47:45 -0600

    added getRandomProduct function to replace repeat code

--- OnlineUser.java
+++ OnlineUser.java
@@ -116,26 +116,24 @@ public class OnlineUser {
                 ResultSet catalog = statement2.executeQuery(sqlQuery4);
                 if(catalog.next()){
                     // Recommend random product from other users in the same location
-                    catalog.last();
-                    int size = catalog.getRow();
-                    Random rand = new Random();
-                    int int_random = rand.nextInt(size) + 1;
-                    catalog.absolute(int_random);
-                    String product_id = catalog.getString("product_id");
+                    String product_id = getRandomProduct(catalog, connection);
+                    if (product_id.contentEquals("Error.")){
+                        output = "Error.";
+                    } else {
                     output = "Here's a product purchased in your area!\nProduct ID: " + product_id;
+                    }
                 } else {
                     // Get nearest 10 orders closest to customer zipcode
                     String sqlQuery5 = "SELECT product_id, product_quantity FROM orders WHERE cust_location != " + zipcode + " ORDER BY ABS(cust_location - " + zipcode + ") LIMIT 10";
                     PreparedStatement statement3 = connection.prepareStatement(sqlQuery5, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
                     ResultSet neighbor = statement3.executeQuery(sqlQuery5);
                     
-                    neighbor.last();
-                    int size = neighbor.getRow();
-                    Random rand = new Random();
-                    int int_random = rand.nextInt(size) + 1;
-                    neighbor.absolute(int_random);
-                    String product_id = neighbor.getString("product_id");
+                    String product_id = getRandomProduct(neighbor, connection);
+                    if (product_id.contentEquals("Error.")){
+                        output = "Error.";
+                    } else {
                     output = "Here's a product purchased near your area!\nProduct ID: " + product_id;
+                    }
                 }
             } else {
                 // find random product to get started
@@ -143,13 +141,12 @@ public class OnlineUser {
                 PreparedStatement statement2 = connection.prepareStatement(sqlQuery4, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
                 ResultSet catalog = statement2.executeQuery(sqlQuery4);
 
-                catalog.last();
-                int size = catalog.getRow();
-                Random rand = new Random();
-                int int_random = rand.nextInt(size) + 1;
-                catalog.absolute(int_random);
-                String product_id = catalog.getString("product_id");
-                output = "Looks like you haven't made a purchase yet. Here's something to get you started!\nProduct ID: " + product_id;
+                String product_id = getRandomProduct(catalog, connection);
+                if (product_id.contentEquals("Error.")){
+                    output = "Error.";
+                } else {
+                    output = "Looks like you haven't made a purchase yet. Here's something to get you started!\nProduct ID: " + product_id;
+                }
             }
             return output;
         } catch (SQLException ex) {
@@ -183,6 +180,26 @@ public class OnlineUser {
                 output += text;
             }
             return output;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return "Error.";
+    }
+
+    public String getRandomProduct (ResultSet catalog, Connection connection){
+        try{
+            catalog.last();
+            int size = catalog.getRow();
+            Random rand = new Random();
+            int int_random = rand.nextInt(size) + 1;
+            catalog.absolute(int_random);
+            String product_id = catalog.getString("product_id");
+            return product_id;
         } catch (SQLException ex) {
             ex.printStackTrace();
             try {
