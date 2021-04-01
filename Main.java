c804c13128924cb18051873fd5c97c8c5f11f683
jfelici4 <jfelici3@msudenver.edu>
2021-04-01 15:21:43 -0600

    Switches == to .equals() in the main method as well

--- Main.java
+++ Main.java
@@ -224,7 +224,7 @@ public class Main {
 	public static void ordersTable(Database db) throws ParseException, SQLException{
 
 		String notDone = "";
-		while(notDone != "N") {
+		while(!notDone.equals("N")) {
 		System.out.println("Would you like to edit your CSV file? (Y/N)");
 		CustomerReplyandCancel test = new CustomerReplyandCancel(db.getUsername(), db.getPassword());
 		String editReply = input.nextLine();
