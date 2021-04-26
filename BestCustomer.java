2b50655a9f409859c4ad27716065331156069ebd
meddy7 <mckennaeddy@gmail.com>
2021-04-25 20:53:35 -0600

    Added in days to generate report

--- BestCustomer.java
+++ BestCustomer.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class BestCustomer extends Database{
 	
@@ -15,18 +16,24 @@ public class BestCustomer extends Database{
 		super(user,pass);
 	}
 	
-	public void customerReport() throws SQLException {
+	public void customerReport() throws SQLException, FileNotFoundException {
 		try {
 			Statement s = connection.createStatement();
 			s.executeUpdate("TRUNCATE test.customers");
 			String email = "SELECT cust_email from orders";
 			PreparedStatement statement = connection.prepareStatement(email);
 			ResultSet set = statement.executeQuery(email);
+			String home = System.getProperty("user.home");
+			int days;
+		    System.out.println("This tool allows you to generate a customer report for a given amount of days. \nFor example, entering '7' will generate a report for the best-selling products in the last 7 days.");
+		    System.out.println("Enter number of days to generate report on: ");
+		    Scanner dayInput = new Scanner(System.in);
+		    days = dayInput.nextInt();
 			while(set.next()) {
 				String emails = set.getString("cust_email");
 				String newCust = "INSERT IGNORE INTO customers (email, purchased) VALUES (?,?) ";
 				PreparedStatement statement3 = connection.prepareStatement(newCust);
-				String sql = "SELECT cust_email, SUM(subtotal) AS purchased FROM orders WHERE cust_email = '"+emails+"' ORDER BY SUM(subtotal)";
+				String sql = "SELECT cust_email, SUM(subtotal) AS purchased FROM orders WHERE cust_email = '"+emails+"' AND date>= DATE_ADD(CURDATE(), INTERVAL -" + days + " DAY) GROUP BY cust_email ORDER BY SUM(subtotal)";
 				PreparedStatement s2 = connection.prepareStatement(sql);
 				ResultSet rs = s2.executeQuery();
 				while(rs.next()) {
@@ -36,45 +43,31 @@ public class BestCustomer extends Database{
 					statement3.addBatch();
 			        statement3.executeBatch();
 			        connection.commit();
-				}	
-			}
-			exportCustomerReport();
-		} catch (SQLException ex) {
-            ex.printStackTrace();
-            try {
-                connection.rollback();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-		}
-	}
-	
-	public void exportCustomerReport () {
-		 String home = System.getProperty("user.home");
-	        try {
-	            PrintWriter pw = new PrintWriter(new File(home + "\\Downloads\\CustomerOrderReport.csv"));
+				}
+				PrintWriter pw = new PrintWriter(new File(home + "\\Downloads\\CustomerOrderReport.csv"));
 	            StringBuilder sb = new StringBuilder();
-	            ResultSet rs = null;
+	            ResultSet rrs = null;
 	            sb.append("Email");
 	            sb.append(",");
 	            sb.append("Purchased");
 	            sb.append("\r\n");
-	            String query = "select * from customers ORDER BY purchased DESC";
+	            String query = "SELECT * FROM customers";
 	            PreparedStatement ps = connection.prepareStatement(query);
-	            rs = ps.executeQuery();
-	            while (rs.next()) {
-	                sb.append(rs.getString("email"));
+	            rrs = ps.executeQuery();
+	            while (rrs.next()) {
+	                sb.append(rrs.getString("email"));
 	                sb.append(",");
-	                sb.append(rs.getString("purchased"));
+	                sb.append(rrs.getString("purchased"));
 	                sb.append("\r\n");
 	            }
 	            pw.write(sb.toString());
 	            pw.close();
-	            System.out.println("Export complete. Find CustomerOrderReport.csv in your Downloads folder.");
-	        } catch (FileNotFoundException e) {
-	        	e.printStackTrace();
-	        } catch (SQLException e) {
-	        	e.printStackTrace();
+			}	
+			System.out.println(days + " day report export complete. Find CustomerOrderReport.csv in your Downloads folder.");
+		} catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
 		}
 	}
 }
